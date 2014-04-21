f5e739d046205c8aa973e051f7d2f91f2a2178c6
vova07 <bazillio07@yandex.ru>
2014-04-21 14:16:34 +0300

    Initial commit

---
+++ ConsoleRunner.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace vova07\console;
+
+use Yii;
+use yii\base\Component;
+use yii\base\InvalidConfigException;
+
+/**
+ * ConsoleRunner - a component for running console commands on background.
+ *
+ * Usage:
+ * ```
+ * ...
+ * $cr = new ConsoleRunner(['file' => '@my/path/to/yii']);
+ * $cr->run('controller/action param1 param2 ...');
+ * ...
+ * ```
+ * or use it like an application component:
+ * ```
+ * // config.php
+ * ...
+ * components [
+ *     'consoleRunner' => [
+ *         'class' => 'vova07\console\ConsoleRunner',
+ *         'file' => '@my/path/to/yii' // or an absolute path to console file
+ *     ]
+ * ]
+ * ...
+ *
+ * // some-file.php
+ * Yii::$app->consoleRunner->run('controller/action param1 param2 ...');
+ * ```
+ */
+class ConsoleRunner extends Component
+{
+    /**
+     * @var string Console application file that will be executed.
+     * Usually it can be `yii` file.
+     */
+    public $file;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        if ($this->file === null) {
+            throw new InvalidConfigException('The "file" property must be set.');
+        }
+    }
+
+    /**
+     * Running console command on background
+     *
+     * @param string $cmd Argument that will be passed to console application
+     * @return boolean
+     */
+    public function run($cmd)
+    {
+        $cmd = PHP_BINDIR . '/php ' . Yii::getAlias($this->file) . ' ' . $cmd;
+        if ($this->isWindows() === true) {
+            pclose(popen('start /b ' . $cmd, 'r'));
+        } else {
+            pclose(popen($cmd . ' /dev/null &', 'r'));
+        }
+        return true;
+    }
+
+    /**
+     * Check operating system
+     *
+     * @return boolean true if it's Windows OS
+     */
+    protected function isWindows()
+    {
+        if (PHP_OS == 'WINNT' || PHP_OS == 'WIN32') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,43 @@
-yii2-console-runner-extension
-=============================
+Console Runner
+==============
 
 An extension for running console commands on background in Yii framework.
+
+Installation
+------------
+
+Add the following to `require` section of your `composer.json`:
+
+```
+"vova07/yii2-console-runner-extension": "*"
+```
+
+Then do `composer install`.
+
+Usage
+-----
+
+Imported class:
+
+```php
+use vova07\console\ConsoleRunner;
+$cr = new ConsoleRunner(['file' => '@my/path/to/yii']);
+$cr->run('controller/action param1 param2 ...');
+```
+
+Application component:
+
+```php
+// config.php
+...
+components [
+    'consoleRunner' => [
+        'class' => 'vova07\console\ConsoleRunner',
+        'file' => '@my/path/to/yii' // or an absolute path to console file
+    ]
+]
+...
+
+// some-file.php
+Yii::$app->consoleRunner->run('controller/action param1 param2 ...');
+```
